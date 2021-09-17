cc3c964d64602312f1ae84c5d00cb238f4f0090f
afa28 <andifahruddinakas@gmail.com>
2021-09-18 06:10:26 +0800

    lang indonesian

---
+++ system/language/indonesian/form_validation_lang.php
@@ -0,0 +1,25 @@
+<?php
+
+$lang['required'] = " %s dibutuhkan.";
+$lang['isset'] = " %s harus memiliki nilai.";
+$lang['valid_email'] = " %s harus berisi alamat email yang sah.";
+$lang['valid_emails'] = " %s harus berisi semua alamat email yang sah.";
+$lang['valid_url'] = " %s harus berisi URL yang sah.";
+$lang['valid_ip'] = " %s harus berisi IP yang sah.";
+$lang['min_length'] = " %s setidaknya harus diisi %s karakter.";
+$lang['max_length'] = " %s panjangnya tidak boleh melebihi %s karakter.";
+$lang['exact_length'] = " %s harus diisi tepat %s karakter.";
+$lang['alpha'] = " %s hanya boleh diisi karakter alfabet";
+$lang['alpha_numeric'] = " %s hanya boleh diisi karakter alfa-numerik.";
+$lang['alpha_dash'] = " %s hanya boleh berisi karakter alfa-numerik, garis bawah, dan tanda hubung.";
+$lang['numeric'] = " %s harus hanya berisi angka.";
+$lang['is_numeric']	= " %s harus berisi karakter numerik.";
+$lang['integer'] = " %s harus berisi integer.";
+$lang['regex_match'] = " %s tidak dalam format yang benar.";
+$lang['matches'] = " %s tidak cocok dengan  %s.";
+$lang['is_unique'] = "%s sudah digunakan.";
+$lang['is_natural']	= " %s harus hanya mengandung angka positif.";
+$lang['is_natural_no_zero']	= " %s harus hanya berisi angka yang lebih besar dari nol.";
+$lang['decimal'] = " %s harus berisi angka desimal.";
+$lang['less_than'] = " %s harus berisi angka yang kurang dari %s.";
+$lang['greater_than'] = " %s harus berisi angka yang lebih besar dari %s.";
